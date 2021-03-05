62fd24208eeb77979f460a764ce5359388eea6d3
Ice_trooper <trooperice@gmail.com>
2021-03-05 17:33:07 +0100

    player script refactor

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -5,44 +5,46 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField, Range(0.1f, 10f)] private float speed;
-    [HideInInspector] public Vector3 direction = new Vector3(0, 0, 0);
     
-    [Header("Roatation properties")] 
-    [SerializeField, Range(1f, 50f)] private float smoothingSpeed = 20f;
-    
+    [Header("Rotation properties")] 
+    [SerializeField, Range(1f, 50f)] private float rotationSpeed = 20f;
     [SerializeField, Range(0.01f, 1f)] private float swingPower = 0.3f;
 
+    private Vector3 _movementDirection = Vector3.zero;
     // Variable ot keep last direction (for use when player is no longer moving)
-    private Vector3 _lastDirection = new Vector3(0, 0, 1);
+    private Vector3 _lastMovementDirection = Vector3.forward;
 
-    void Start()
+    private void Update()
     {
-    }
-
-    void Update()
-    {
-        // Quaternion that hold rotation for character model
-        var quaternion = Quaternion.LookRotation(_lastDirection);
-        
-        // If object is in move
-        if (direction.magnitude >= 0.1f)
-        {
-            // Change rotation according to character movement direction + swing
-            quaternion = Quaternion.LookRotation(direction + new Vector3(0f, -swingPower, 0f));
-            _lastDirection = direction;
-            controller.Move(direction * (speed * Time.deltaTime));
-        }
-        
-        // Apply rotation
-        var rotation = transform.rotation;
-        rotation = Quaternion.Slerp(rotation, quaternion, smoothingSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(rotation.eulerAngles.x, rotation.eulerAngles.y, rotation.eulerAngles.z);
+        Move();
     }
 
     // Method invoked by Player Input Component when input associated with movement changed
-    public void Movement(InputAction.CallbackContext context)
+    public void OnMovementInput(InputAction.CallbackContext context)
     {
         var inputMovement = context.ReadValue<Vector2>();
-        direction = new Vector3(inputMovement.x, 0, inputMovement.y).normalized;
+        _movementDirection = new Vector3(inputMovement.x, _movementDirection.y, inputMovement.y);
+    }
+
+    private void Move()
+    {
+        // New rotation for the character
+        Quaternion newRotation;
+
+        // If object is in move
+        if(_movementDirection != Vector3.zero)
+        {
+            // Change rotation according to character movement direction + swing
+            newRotation = Quaternion.LookRotation(_movementDirection + new Vector3(0f, -swingPower, 0f));
+            controller.Move(_movementDirection * (speed * Time.deltaTime));
+            _lastMovementDirection = _movementDirection;
+        }
+        else
+        {
+            newRotation = Quaternion.LookRotation(_lastMovementDirection);
+        }
+
+        // Apply rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
     }
 }
