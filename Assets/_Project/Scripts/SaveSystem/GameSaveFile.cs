54791e07f00978f22056e7d8931ffd99c036fba6
Kamil Bąk <kamilbak113@gmail.com>
2021-03-19 17:39:49 +0100

    reseting project

--- Assets/_Project/Scripts/SaveSystem/GameSaveFile.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class GameSaveFile
-{
-    public int currentLevelId = 1;
-    public float audioVolume = 100f;
-    public float musicVolume = 100f;
-    public float sfxVolume = 100f;
-}
--- Assets/_Project/Scripts/VoltageData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class VoltageData
-{
-    public float value;
-    public Color color;
-}
