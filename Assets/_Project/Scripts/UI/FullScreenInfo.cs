e41057b74a9e810da68a00dcec6cb59907812808
Kamil Bąk <kamilbak113@gmail.com>
2021-04-26 08:45:41 +0200

    added MyBox library; added correct version info in the credits;

--- Assets/_Project/Scripts/UI/FullScreenInfo.cs
+++ Assets/_Project/Scripts/UI/FullScreenInfo.cs
@@ -1,13 +1,22 @@
 using DG.Tweening;
+using MyBox;
+using TMPro;
 using UnityEngine;
 
 public class FullScreenInfo : MonoBehaviour
 {
     private CanvasGroup _canvasGroup;
+    [SerializeField] private bool appendVersionAtTheEnd = false;
+
+    [ConditionalField("appendVersionAtTheEnd"), SerializeField] private TextMeshProUGUI TMP;
 
     private void Start()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+        if (appendVersionAtTheEnd)
+        {
+            TMP.text += Application.version;
+        }
     }
 
     public void Show()
